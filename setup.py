bd95fea71e57e98d6d2372076107b9e10c878601
Aden Herold <aden.herold1@gmail.com>
2019-08-20 22:52:23 +0930

    Added setup.py file to publish package on pypi

---
+++ setup.py
@@ -0,0 +1,29 @@
+import os
+import setuptools
+
+with open('README.md') as file:
+    long_description = file.read()
+
+setuptools.setup(
+    name="django-typomatic",
+    version="0.0.1",
+    url="https://github.com/adenh93/django_typomatic",
+
+    author="Aden Herold",
+    author_email="aden.herold1@gmail.com",
+
+    description="A simple solution for generating Typescript interfaces from your Django Rest Framework Serializers.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords=['Django', 'Django Rest Framework', 'DRF', 'Typescript', 'Python'],
+
+    packages=['django_typomatic'],
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+
+    install_requires=[
+        'django',
+        'django_rest_framework'
+    ]
+)
