bd316ddf635f80406d44262b29758e1933b171bf
Aden Herold <aden.herold1@gmail.com>
2020-11-05 08:43:19 +1100

    Project: Bumped package version to v1.2.0 in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as file:
 
 setuptools.setup(
     name="django-typomatic",
-    version="1.1.0",
+    version="1.2.0",
     url="https://github.com/adenh93/django-typomatic",
 
     author="Aden Herold",
