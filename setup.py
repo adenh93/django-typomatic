be4e2e161af87a966d698c1813009f16b07939df
Aden Herold <aden.herold1@gmail.com>
2021-04-08 09:26:45 +1000

    Project: Bumped package version to 1.3.0 in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as file:
 
 setuptools.setup(
     name="django-typomatic",
-    version="1.2.1",
+    version="1.3.0",
     url="https://github.com/adenh93/django-typomatic",
 
     author="Aden Herold",
