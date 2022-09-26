48a9ae25e04ad561be75eee0a7ace837e6ae4d09
aden herold <adenherold@DESKTOP-LRQH2FQ.localdomain>
2022-09-26 19:29:59 +1000

    build: bumped package version to 1.6.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as file:
 
 setuptools.setup(
     name="django-typomatic",
-    version="1.6.2",
+    version="1.6.3",
     url="https://github.com/adenh93/django-typomatic",
 
     author="Aden Herold",
