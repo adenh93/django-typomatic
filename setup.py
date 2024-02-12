4090351d0426c6b72ca4cfb3c7129e59ad38bead
Aden Herold <48753915+adenh93@users.noreply.github.com>
2024-02-13 09:20:19 +1100

    build: bump version to 2.5.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as file:
 
 setuptools.setup(
     name="django-typomatic",
-    version="2.4.2",
+    version="2.5.0",
     url="https://github.com/adenh93/django-typomatic",
 
     author="Aden Herold",
